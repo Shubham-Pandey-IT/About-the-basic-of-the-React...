96ab5fb6ba077531579bb491cf976bce7f197a4e
Shubham Pandey <shubhamp7282@gmail.com>
2024-08-03 13:25:32 +0530

    basic

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ points to remember.....
 createElement is not just create an element/html , it just create an object that appears as an HTML element ,
 when we are going to render as root.render(heading)  then, it replace the previous data that is present there..
 
+
+NODE_MODULES----------------->>>>>>
+    node_modules are one of the most important directories in your React project as React requires node_modules to run. The node_modules directory is where all the dependancies packages are stored that are used to build and run your react project.
+
+
--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-/* <div>
-    <div>
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-
-
-
-// Create a React element
- const heading = React.createElement(
-    "div", {id:"parent"}, 
-        React.createElement(
-        "div", {id:"children"}, 
-            [React.createElement(
-                "h1", {}, 
-                "Hello I'm H1 Tag!!"
-            ),
-            React.createElement(
-                "h2", {}, 
-                "Hello I'm H2 Tag!!"
-            )
+const heading = React.createElement("div", {id:"parent"}, 
+        React.createElement("div", {id:"children"}, 
+        [   
+            React.createElement("h1", {}, "Hello I'm H1 Tag!!"),
+            React.createElement("h2", {}, "Hello I'm H2 Tag!!")
         ]
     )
 );
 
-//  // Get the root element by ID
- const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
- // Render the React element
- root.render(heading);
+root.render(heading);
